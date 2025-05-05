fb74dda4f6b212dac409744937bc8efd07493af8
joseph-grabinger <joseph.grabinger@yahoo.de>
2025-05-05 16:02:44 +0200

    Add URL extension to read remaining routes with args

--- Sources/Coordinator/Extensions/URL+Extensions.swift
+++ Sources/Coordinator/Extensions/URL+Extensions.swift
@@ -16,4 +16,20 @@ extension URL {
         routes.append(contentsOf: pathComponents.filter { $0 != "/" })
         return routes
     }
+    
+    /// Returns all remaining route components from the URL, including the host and path segments including query arguments.
+    ///
+    /// This effectively drops the URL's scheme from the absolute string and splits at the remaining string at each slash.
+    ///
+    /// - Returns: An array of `String` representing the host and each path component.
+    func remainingRoutesWithArgs() -> [String] {
+        var url: String?
+        if let scheme {
+            url = String(absoluteString.dropFirst(scheme.count + 3))
+        } else {
+            url = absoluteString
+        }
+        
+        return url?.components(separatedBy: "/") ?? []
+    }
 }
