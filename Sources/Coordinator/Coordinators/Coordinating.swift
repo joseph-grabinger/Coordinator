8c002660fa32f136efb71291580ca6bad90bef9e
joseph-grabinger <joseph.grabinger@yahoo.de>
2025-08-09 17:53:49 +0200

    Remove redundant Equatable conformance

--- Sources/Coordinator/Coordinators/Coordinating.swift
+++ Sources/Coordinator/Coordinators/Coordinating.swift
@@ -9,7 +9,7 @@ import Foundation
 
 /// Base protocol that defines the shared behavior and identity requirements for all coordinators.
 @MainActor
-public protocol Coordinating: ObservableObject, Identifiable, Hashable, Equatable, CustomStringConvertible {
+public protocol Coordinating: ObservableObject, Identifiable, Hashable, CustomStringConvertible {
 
     /// The unique identifier of the coordinator.
     nonisolated var id: String { get }
