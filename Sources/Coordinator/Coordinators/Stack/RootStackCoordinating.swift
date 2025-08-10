3ad59d89bfc437883ffc8140b8451c3b8756bd27
joseph-grabinger <joseph.grabinger@yahoo.de>
2025-08-10 18:37:14 +0200

    Introduce transition points

--- Sources/Coordinator/Coordinators/Stack/RootStackCoordinating.swift
+++ Sources/Coordinator/Coordinators/Stack/RootStackCoordinating.swift
@@ -14,7 +14,20 @@ public final class RootStackCoordinator<R: Routable>: StackNavigating {
 	// MARK: - Public Properties
 
 	/// The navigation path representing the current state of navigation.
-	@Published public var path = NavigationPath()
+    @Published public var path = NavigationPath() {
+        didSet {
+            guard path.count < oldValue.count else { return }
+            transitionPoints = transitionPoints.filter { _, index in
+                return index <= path.count - 1
+            }
+        }
+    }
+    
+    /// A dictionary holding the transition points of the pushed coordinators.
+    ///
+    /// The key is the `StackCoordinating`-conforming coordinator's ID.
+    /// And the value corresponds to the index  coordinator's `initialRoute` within the `path`.
+    private(set) var transitionPoints = [String: Int]()
 
 	/// The initial route that this coordinator starts with.
     public let initialRoute: R
@@ -32,6 +45,7 @@ public final class RootStackCoordinator<R: Routable>: StackNavigating {
     
     public func pushCoordinator(_ coordinator: any StackCoordinating) {
         coordinator.root = self
+        transitionPoints[coordinator.id] = path.count
         pushRoute(coordinator.initialRoute)
     }
     
@@ -49,5 +63,6 @@ public final class RootStackCoordinator<R: Routable>: StackNavigating {
     
     public func popToRoot() {
         path = NavigationPath()
+        transitionPoints = [:]
     }
 }
