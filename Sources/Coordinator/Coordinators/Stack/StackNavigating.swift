88bfcbb6c7ea7495148a9b555c42ce6b41d42240
joseph-grabinger <joseph.grabinger@yahoo.de>
2025-08-10 17:54:00 +0200

    Implement StackNavigating protocol

---
+++ Sources/Coordinator/Coordinators/Stack/StackNavigating.swift
@@ -0,0 +1,40 @@
+//
+//  StackNavigating.swift
+//  Coordinator
+//
+//  Created by Joseph Grabinger on 10.08.25.
+//
+
+/// A protocol that defines methods for stack-based navigation.
+@MainActor
+public protocol StackNavigating {
+    
+    // MARK: Methods
+    
+    /// Pushes a new coordinator onto the navigation stack.
+    /// - Parameter coordinator: A `StackCoordinating`-conforming instance to push.
+    func pushCoordinator(_ coordinator: any StackCoordinating)
+    
+    /// Pushes a new `Route` onto the navigation stack.
+    /// - Parameter route: A `Routable`-conforming route to push.
+    func pushRoute<Route: Routable>(_ route: Route)
+    
+    /// Pops the specified number of routes of the navigation stack.
+    /// - Parameter count: The number of routes to pop.
+    func popRoute(count: Int)
+    
+    /// Pops all routes of the navigation stack.
+    ///
+    /// This effectively resets the stack to its initial route.
+    func popToRoot()
+}
+
+// MARK: - Convenience Methods
+
+public extension StackNavigating {
+    
+    /// Pops the top-most route of the navigation stack.
+    func popRoute() {
+        self.popRoute(count: 1)
+    }
+}
