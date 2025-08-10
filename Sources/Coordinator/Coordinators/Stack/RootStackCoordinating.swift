ec2dbc440ee821e0c13c6412ac9666b7cf047c67
joseph-grabinger <joseph.grabinger@yahoo.de>
2025-08-10 18:57:55 +0200

    Add convenience methods to StackNavigating protocol

--- Sources/Coordinator/Coordinators/Stack/RootStackCoordinating.swift
+++ Sources/Coordinator/Coordinators/Stack/RootStackCoordinating.swift
@@ -10,50 +10,53 @@ import OSLog
 
 /// A concrete implementation of `RootStackCoordinating` that manages the `NavigationPath` and initial route.
 public final class RootStackCoordinator<R: Routable>: StackNavigating {
-
-	// MARK: - Public Properties
-
-	/// The navigation path representing the current state of navigation.
+    
+    // MARK: - Public Properties
+    
+    /// The navigation path representing the current state of navigation.
     @Published public var path = NavigationPath() {
         didSet {
             guard path.count < oldValue.count else { return }
-            transitionPoints = transitionPoints.filter { _, index in
+            transitionIndices = transitionIndices.filter { _, index in
                 return index <= path.count - 1
             }
         }
     }
     
-    /// A dictionary holding the transition points of the pushed coordinators.
+    /// A dictionary holding the transition indices of the pushed coordinators.
     ///
     /// The key is the `StackCoordinating`-conforming coordinator's ID.
-    /// And the value corresponds to the index  coordinator's `initialRoute` within the `path`.
-    private(set) var transitionPoints = [String: Int]()
-
-	/// The initial route that this coordinator starts with.
+    /// And the value corresponds to the index of the coordinator's `initialRoute` within the `path`.
+    private(set) var transitionIndices = [String: Int]()
+    
+    /// The initial route that this coordinator starts with.
     public let initialRoute: R
-
-	// MARK: - Initialization
-
-	/// Initializes a new `RootStackCoordinator` with the  given coordinator.
-	/// - Parameter coordinator: A initial stack-based coordinator.
-	public init<C: StackCoordinating>(coordinator: C) where C.Route == R {
-		self.initialRoute = coordinator.initialRoute
-		coordinator.root = self
-	}
     
-    // MARK: Public Methods
+    // MARK: - Initialization
     
-    public func pushCoordinator(_ coordinator: any StackCoordinating) {
+    /// Initializes a new `RootStackCoordinator` with the  given coordinator.
+    /// - Parameter coordinator: A initial stack-based coordinator.
+    public init<C: StackCoordinating>(coordinator: C) where C.Route == R {
+        self.initialRoute = coordinator.initialRoute
         coordinator.root = self
-        transitionPoints[coordinator.id] = path.count
+    }
+}
+
+// MARK: StackNavigating
+
+public extension RootStackCoordinator {
+    
+    func pushCoordinator(_ coordinator: any StackCoordinating) {
+        coordinator.root = self
+        transitionIndices[coordinator.id] = path.count
         pushRoute(coordinator.initialRoute)
     }
     
-    public func pushRoute<Route: Routable>(_ route: Route) where Route : Routable {
+    func pushRoute<Route: Routable>(_ route: Route) where Route : Routable {
         path.append(route)
     }
     
-    public func popRoute(count: Int) {
+    func popRoute(count: Int) {
         guard path.count >= count else {
             Logger.coordinator.warning("\(self) cannot pop \(count) routes: path contains only \(self.path.count).")
             return
@@ -61,8 +64,26 @@ public final class RootStackCoordinator<R: Routable>: StackNavigating {
         path.removeLast(count)
     }
     
-    public func popToRoot() {
+    func popCoordinator(_ coordinator: any StackCoordinating) {
+        guard let transitionIndex = transitionIndices[coordinator.id] else {
+            Logger.coordinator.warning("\(self) cannot pop coordinator \(coordinator.description): no transition index found.")
+            return
+        }
+        let routesToPop = path.count - transitionIndex - 1
+        popRoute(count: routesToPop)
+    }
+    
+    func popToInitialRoute(of coordinator: any StackCoordinating) {
+        guard let transitionIndex = transitionIndices[coordinator.id] else {
+            Logger.coordinator.warning("\(self) cannot pop to initial route of \(coordinator.description): no transition index found.")
+            return
+        }
+        let routesToPop = path.count - transitionIndex
+        popRoute(count: routesToPop)
+    }
+    
+    func popToRoot() {
         path = NavigationPath()
-        transitionPoints = [:]
+        transitionIndices = [:]
     }
 }
--- Sources/Coordinator/Coordinators/Stack/StackNavigating.swift
+++ Sources/Coordinator/Coordinators/Stack/StackNavigating.swift
@@ -14,16 +14,27 @@ public protocol StackNavigating: ObservableObject, CustomStringConvertible {
     // MARK: Methods
     
     /// Pushes a new coordinator onto the navigation stack.
-    /// - Parameter coordinator: A `StackCoordinating`-conforming instance to push.
+    /// - Parameter coordinator: The `StackCoordinating`-conforming instance to push.
     func pushCoordinator(_ coordinator: any StackCoordinating)
     
     /// Pushes a new `Route` onto the navigation stack.
-    /// - Parameter route: A `Routable`-conforming route to push.
+    /// - Parameter route: The `Routable`-conforming route to push.
     func pushRoute<Route: Routable>(_ route: Route)
     
     /// Pops the specified number of routes of the navigation stack.
     /// - Parameter count: The number of routes to pop.
     func popRoute(count: Int)
+    
+    /// Pops the current coordinator of the navigation stack.
+    /// 
+    /// This effectively displays the lat route before the given coordinator.
+    ///
+    /// - Parameter coordinator: The `StackCoordinating`-conforming instance to pop.
+    func popCoordinator(_ coordinator: any StackCoordinating)
+    
+    /// Pops all routes until the initial route of the given coordinator of the navigation stack.
+    /// - Parameter coordinator: The `StackCoordinating`-conforming instance to whose initial route to pop.
+    func popToInitialRoute(of coordinator: any StackCoordinating)
     
     /// Pops all routes of the navigation stack.
     ///
